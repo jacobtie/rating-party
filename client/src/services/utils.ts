fc2bdce4a4aeb776b089372f9c59be2e7bba659e
Jacob Krevat <jacobtie@gmail.com>
2023-07-07 11:40:19 -0400

    Updating prod api endpoint

--- client/src/services/utils.ts
+++ client/src/services/utils.ts
@@ -1,5 +1,5 @@
 export let baseUrl = 'http://localhost:3000/api/v1';
 
 if (window.location.protocol === 'https:') {
-  baseUrl = 'https://localhost:3000/api/v1'; // TODO: change to production url
+  baseUrl = 'https://rating-party.onrender.com/api/v1'; // TODO: change to production url
 }
